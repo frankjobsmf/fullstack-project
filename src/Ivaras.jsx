ec65cbb4188d0a9e4a06c4acdabd01d703df11ef
Francisco Uribe <franciscouribematus@gmail.com>
2022-07-13 13:53:52 -0400

    primer commit

---
+++ src/Ivaras.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Ivaras = () => {
+  return (
+    <>
+
+        <h1>Proyecto FullStack Ivaras</h1>
+
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Ivaras } from './Ivaras'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Ivaras />
+  </React.StrictMode>
+)
